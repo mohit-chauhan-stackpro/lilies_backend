a662017798ce696dd2b04894393ca48ded04b603
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-07-27 14:05:18 +0530

    created ItemDetail model

--- userdetail/models.py
+++ userdetail/models.py
@@ -12,7 +12,16 @@ class Cart(models.Model):
 
 class Order(models.Model):
     img_path = models.CharField()
-    item_name = models.CharField(max_length=12)
+    order_id = models.CharField(max_length=12)
     quantity = models.IntegerField()
     total = models.IntegerField()
     status = models.CharField()
+
+
+class ItemDetail(models.Model):
+    img_path = models.CharField()
+    item_name = models.CharField(max_length=30)
+    item_description = models.TextField(max_length=200)
+    pices_available = models.IntegerField()
+    unit_price = models.IntegerField()
+    time_to_cook = models.CharField()
