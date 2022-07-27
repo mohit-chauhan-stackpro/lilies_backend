7082cdd3c268d3be2227ae993d275c802b0e3819
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-07-27 15:09:06 +0530

    added forigen key for user and itemdetail

--- userdetail/models.py
+++ userdetail/models.py
@@ -1,21 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-
-class Cart(models.Model):
-    img_path = models.CharField()
-    item_name = models.CharField(max_length=30)
-    quantity = models.IntegerField()
-    unit_price = models.IntegerField()
-
-
-class Order(models.Model):
-    img_path = models.CharField()
-    order_id = models.CharField(max_length=12)
-    quantity = models.IntegerField()
-    total = models.IntegerField()
-    status = models.CharField()
 
 
 class ItemDetail(models.Model):
@@ -25,3 +11,18 @@ class ItemDetail(models.Model):
     pices_available = models.IntegerField()
     unit_price = models.IntegerField()
     time_to_cook = models.CharField()
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    food_item = models.ForeignKey(ItemDetail, on_delete=models.SET_NULL)
+    quantity = models.IntegerField()
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    img_path = models.CharField()
+    order_id = models.CharField(max_length=12)
+    quantity = models.IntegerField()
+    total = models.IntegerField()
+    status = models.CharField()
