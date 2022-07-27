38cd6f2daa73c6ba143f732bde1073fb7f42bffc
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-07-27 14:01:41 +0530

    created model for Cart

--- userdetail/models.py
+++ userdetail/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cart(models.Model):
+    img_path = models.CharField()
+    item_name = models.CharField(max_length=30)
+    quantity = models.IntegerField()
+    unit_price = models.IntegerField()
